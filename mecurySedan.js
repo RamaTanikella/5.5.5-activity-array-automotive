16d60053b829143ecd7b328d793a11027e651816
RamaTanikella <88115546+RamaTanikella@users.noreply.github.com>
2021-11-06 00:07:18 -0400

    array automotive solution

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,3 +1,51 @@
+import Vehicle from "./vehicleBaseClass.js"; 
+
+class Car extends Vehicle {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 6;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 200;
+        this.fuel = 100;
+        this.scheduleService = false;
+    }
+
+    start() {
+        if (this.fuel > 0) {            
+            console.log("engine has started.");
+        } else {
+            return this.started = false;
+            console.log("no fuel");
+        }
+    }
+
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;               
+            } else {
+                console.log(this.model + " " + this.make + "full");
+
+            }
+        } else {
+            console.log(this.model + " " + this.make + " is full");
+        }
+    }
+
+    scheduleService(mileage) {
+        if (this.mileage > 30000) {            
+            this.scheduleService == true
+            return this.scheduleService;                       
+         }
+}
+
+
+
+
+
+
 //this includes the vehicle class as a module
 const VehicleModule = require("./vehicle")
 
