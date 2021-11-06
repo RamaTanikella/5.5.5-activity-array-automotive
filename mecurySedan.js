3ccdd7da051f3f5fdecf236feadd3ed417e3fc77
RamaTanikella <88115546+RamaTanikella@users.noreply.github.com>
2021-11-06 00:10:28 -0400

    array auto solution

--- mecurySedan.js
+++ mecurySedan.js
@@ -47,7 +47,7 @@ class Car extends Vehicle {
 
 
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicle")
+const VehicleModule = require("./vehicleBaseClass")
 
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
